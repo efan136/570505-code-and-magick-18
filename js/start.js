dc7e63d0549dfde2f6a36a4c890c0a51ca6e62fc
Andrew <andrei_efremov@inbox.ru>
2019-09-26 00:17:43 +0400

    made function for  every task

--- js/start.js
+++ js/start.js
@@ -32,50 +32,51 @@ var getMaxElement = function (arr) {
   return maxElement;
 }; // МАКС ЭЛЕМЕНТ
 
-var getRandomPercent = function() {
+var getRandomPercent = function () {
   var randomPercent = (Math.random() * 100);
-  return randomPercent
+  return randomPercent;
 }; // СЛУЧАЙНОЕ ЧИСЛО ДЛЯ ПОРКАСКИ КОЛОНОК
 
-var printNames = function(ctx, arr) {
+var printNames = function (ctx, arr) {
   for (var k = 0; k <= arr.length - 1; k++) {
     ctx.fillText(arr[k], CLOUD_X + GAP + (COLL_WIDTH * k) + (COLL_SPACE * k), FONT_Y + FONT_HEIGHT + GAP_FONT + FONT_HEIGHT + GAP_FONT + GRAPH_HEIGHT);
   }
 }; // ПЕЧАТЬ ИМЕН
 
-var getCollHeights = function(arr) {
+var getCollHeights = function (arr) {
   var collHeights = [];
   for (var m = 0; m <= arr.length - 1; m++) {
     var collHeight = (maxCollHeight * arr[m]) / getMaxElement(arr);
     collHeights.push(collHeight);
-  };
+  }
   return collHeights;
 }; // ВЫНЕС В ОТДЕЛЬНУЮ Ф-Ю ТК ЗНАЧЕНИЕ НУЖНО В 2 МЕСТАХ - ДЛЯ ОТРИСОВКИ КОЛОНОК И ДЛЯ ПЕЧАТИ ЗНАЧЕНИЙ НАД КОЛОНКОЙ
 
-var drawGraph = function(ctx, times, names) {
+var drawGraph = function (ctx, times, names) {
+  var collHeightsArray = getCollHeights(times);
   for (var i = 0; i <= times.length - 1; i++) {
-    getCollHeights(times)[i];
+    collHeightsArray[i];
     if (names[i] === 'Вы') {
       ctx.fillStyle = USER_COLOR;
     } else {
       ctx.fillStyle = 'hsl(240,100%,' + getRandomPercent() + '%)';
-    };
+    }
 
     ctx.beginPath();
     ctx.moveTo(CLOUD_X + GAP + (COLL_WIDTH * i) + (COLL_SPACE * i), FONT_Y + FONT_HEIGHT + GAP_FONT + FONT_HEIGHT + GAP_FONT + GRAPH_HEIGHT - FONT_HEIGHT);
-    ctx.lineTo(CLOUD_X + GAP + (COLL_WIDTH * i) + (COLL_SPACE * i), FONT_Y + FONT_HEIGHT + GAP_FONT + FONT_HEIGHT + GAP_FONT + GRAPH_HEIGHT - FONT_HEIGHT - getCollHeights(times)[i]);
-    ctx.lineTo(CLOUD_X + GAP + (COLL_WIDTH * i) + (COLL_SPACE * i) + COLL_WIDTH, FONT_Y + FONT_HEIGHT + GAP_FONT + FONT_HEIGHT + GAP_FONT + GRAPH_HEIGHT - FONT_HEIGHT - getCollHeights(times)[i]);
+    ctx.lineTo(CLOUD_X + GAP + (COLL_WIDTH * i) + (COLL_SPACE * i), FONT_Y + FONT_HEIGHT + GAP_FONT + FONT_HEIGHT + GAP_FONT + GRAPH_HEIGHT - FONT_HEIGHT - collHeightsArray[i]);
+    ctx.lineTo(CLOUD_X + GAP + (COLL_WIDTH * i) + (COLL_SPACE * i) + COLL_WIDTH, FONT_Y + FONT_HEIGHT + GAP_FONT + FONT_HEIGHT + GAP_FONT + GRAPH_HEIGHT - FONT_HEIGHT - collHeightsArray[i]);
     ctx.lineTo(CLOUD_X + GAP + (COLL_WIDTH * i) + (COLL_SPACE * i) + COLL_WIDTH, FONT_Y + FONT_HEIGHT + GAP_FONT + FONT_HEIGHT + GAP_FONT + GRAPH_HEIGHT - FONT_HEIGHT);
     ctx.closePath();
     ctx.fill();
   };
-}; // отрисовка колонок
+} // отрисовка колонок
 
-var printTimes = function(ctx, arr, color) {
+var printTimes = function (ctx, arr) {
   ctx.fillStyle = FONT_COLOR;
   for (var j = 0; j <= arr.length - 1; j++) {
     ctx.fillText(Math.round(arr[j]), CLOUD_X + GAP + (COLL_WIDTH * j) + (COLL_SPACE * j), FONT_Y + FONT_HEIGHT + GAP_FONT + FONT_HEIGHT + GAP_FONT + GRAPH_HEIGHT - FONT_HEIGHT - getCollHeights(arr)[j] - GAP_FONT);
-  };
+  }
 }; // отрисовка значений времени
 
 window.renderStatistics = function (ctx, names, times) {
