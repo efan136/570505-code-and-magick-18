877926593531f8b2b6618deeeffbae134af1be8c
Andrew <andrei_efremov@inbox.ru>
2019-09-29 19:17:59 +0400

    made a function, which prints heading. set vertical direction of functions (heading- value, columns, names)

--- js/start.js
+++ js/start.js
@@ -16,11 +16,12 @@ var maxCollHeight = GRAPH_HEIGHT - FONT_HEIGHT - GAP_FONT - FONT_HEIGHT; // ма
 var FONT_COLOR = '#000000';
 var FONT_STYLE = 'PT Mono 16px';
 var USER_COLOR = 'rgba(255, 0, 0, 1)';
+var headingLines = ['Ура вы победили!', 'Список результатов:'];
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
-}; //  ОБЛАКО С ТЕНЬЮ
+};
 
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
@@ -30,18 +31,31 @@ var getMaxElement = function (arr) {
     }
   }
   return maxElement;
-}; // МАКС ЭЛЕМЕНТ
+};
 
-var getRandomPercent = function () {
-  var randomPercent = (Math.random() * 100);
-  return randomPercent;
-}; // СЛУЧАЙНОЕ ЧИСЛО ДЛЯ ПОРКАСКИ КОЛОНОК
+var getRandomNumber = function () {
+  var randomNumber = (Math.random() * 100);
+  return randomNumber;
+};
 
-var printNames = function (ctx, arr) {
+var printHeading = function (ctx, arr) {
+  ctx.fillStyle = FONT_COLOR;
+  ctx.font = FONT_STYLE;
   for (var i = 0; i <= arr.length - 1; i++) {
-    ctx.fillText(arr[i], CLOUD_X + GAP + (COLL_WIDTH * i) + (COLL_SPACE * i), FONT_Y + FONT_HEIGHT + GAP_FONT + FONT_HEIGHT + GAP_FONT + GRAPH_HEIGHT);
+    if(i < 1) {
+      ctx.fillText(arr[i], CLOUD_X + GAP, FONT_Y);
+    }
+    else {ctx.fillText(arr[i], CLOUD_X + GAP, FONT_Y + (FONT_HEIGHT * i) + (GAP_FONT * i));
+    }
   }
-}; // ПЕЧАТЬ ИМЕН
+};
+
+var printTimes = function (ctx, arr) {
+  ctx.fillStyle = FONT_COLOR;
+  for (var i = 0; i <= arr.length - 1; i++) {
+    ctx.fillText(Math.round(arr[i]), CLOUD_X + GAP + (COLL_WIDTH * i) + (COLL_SPACE * i), FONT_Y + FONT_HEIGHT + GAP_FONT + FONT_HEIGHT + GAP_FONT + GRAPH_HEIGHT - FONT_HEIGHT - getCollHeights(arr)[i] - GAP_FONT);
+  }
+};
 
 var getCollHeights = function (arr) {
   var collHeights = [];
@@ -50,14 +64,14 @@ var getCollHeights = function (arr) {
     collHeights.push(collHeight);
   }
   return collHeights;
-}; // ВЫНЕС В ОТДЕЛЬНУЮ Ф-Ю ТК ЗНАЧЕНИЕ НУЖНО В 2 МЕСТАХ - ДЛЯ ОТРИСОВКИ КОЛОНОК И ДЛЯ ПЕЧАТИ ЗНАЧЕНИЙ НАД КОЛОНКОЙ
+};
 
 var drawGraph = function (ctx, times, names) {
   for (var i = 0; i <= times.length - 1; i++) {
     if (names[i] === 'Вы') {
       ctx.fillStyle = USER_COLOR;
     } else {
-      ctx.fillStyle = 'hsl(240,100%,' + getRandomPercent() + '%)';
+      ctx.fillStyle = 'hsl(240,100%,' + getRandomNumber() + '%)';
     }
 
     ctx.beginPath();
@@ -72,24 +86,20 @@ var drawGraph = function (ctx, times, names) {
     ctx.closePath();
     ctx.fill();
   }
-}; // отрисовка колонок
-
-var printTimes = function (ctx, arr) {
-  ctx.fillStyle = FONT_COLOR;
-  for (var i = 0; i <= arr.length - 1; i++) {
-    ctx.fillText(Math.round(arr[i]), CLOUD_X + GAP + (COLL_WIDTH * i) + (COLL_SPACE * i), FONT_Y + FONT_HEIGHT + GAP_FONT + FONT_HEIGHT + GAP_FONT + GRAPH_HEIGHT - FONT_HEIGHT - getCollHeights(arr)[i] - GAP_FONT);
-  }
-}; // отрисовка значений времени
-
-window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, SHADOW_COLOR); // тень
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR); // облако
-  ctx.fillStyle = FONT_COLOR;
-  ctx.font = FONT_STYLE;
-  ctx.fillText('Ура вы победили!', CLOUD_X + GAP, FONT_Y);
-  ctx.fillText('Список результатов:', CLOUD_X + GAP, FONT_Y + FONT_HEIGHT + GAP_FONT); // текст
-  printNames(ctx, names); // ПЕЧАТЬ ИМЕН
-  drawGraph(ctx, times, names); // ОТРИСОВКА КОЛОНКИ
-  printTimes(ctx, times); // ПЕЧАТЬ ЗНАЧЕНИЯ НАД КОЛОНКОЙ
 };
 
+var printNames = function (ctx, arr) {
+  ctx.fillStyle = FONT_COLOR;
+  for (var i = 0; i <= arr.length - 1; i++) {
+    ctx.fillText(arr[i], CLOUD_X + GAP + (COLL_WIDTH * i) + (COLL_SPACE * i), FONT_Y + FONT_HEIGHT + GAP_FONT + FONT_HEIGHT + GAP_FONT + GRAPH_HEIGHT);
+  }
+};
+
+window.renderStatistics = function (ctx, names, times) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, SHADOW_COLOR);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_COLOR);
+  printHeading(ctx, headingLines);
+  printNames(ctx, names);
+  drawGraph(ctx, times, names);
+  printTimes(ctx, times);
+};
